f1db92dce25dc3a53e51fd3126e13a5d66a50bb3
Britt-hub <britloy91@gmail.com>
2020-11-24 15:39:49 -0500

    making the list and checking it twice

---
+++ List Page/script.js
@@ -0,0 +1,47 @@
+// TO DO LIST
+// create archive
+// data base or local storage
+// grocery api
+// fix file structure 
+
+
+const input = document.getElementById("one-item");
+
+input.addEventListener("keydown", function(e) {
+    if (e.key == "Enter") {
+        addItem();
+    }
+})
+
+function addItem() {
+    const item =" - " + document.getElementById("one-item").value;
+    const h1 = document.createElement("h1");
+
+    document.getElementById("one-item").value = '';
+    // this will append the food that is entered into the input box and placed into a list below after "enter Key" is clicked
+    h1.appendChild(document.createTextNode(item));
+
+
+    //crossing out each item when the item is clicked
+    h1.addEventListener("click", function () {
+        h1.style.textDecoration = "line-through";
+    })
+
+    var div = document.getElementById("all-items");
+    div.insertAdjacentElement("beforeend", h1);
+
+}
+
+function trashList() {
+    var list = document.getElementById("all-items");
+    var i = 0;
+
+    while (i < list.childNodes.length) {
+        list.removeChild(list.childNodes[i]);
+    }
+}
+
+function archiveList() {
+    var list = document.getElementById("all-items");
+
+}
