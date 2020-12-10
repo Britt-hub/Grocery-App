5cb239d4d3dafb1cf52dd43af43a23307713a5f2
Britt-hub <britloy91@gmail.com>
2020-12-09 19:43:01 -0500

    creating the save list action

--- List Page/script.js
+++ List Page/script.js
@@ -4,32 +4,50 @@
 // grocery api
 // fix file structure 
 
+//html UL for add item
+
 
 const input = document.getElementById("one-item");
 const archiveList = document.getElementById("all-items");
+const itemName = document.getElementById("one-item").value;
+const groceryListContainer = document.getElementById("grocery-list");
 
-input.addEventListener("keydown", function(e) {
+let groceryList = [];
+
+input.addEventListener("keydown", function (e) {
     if (e.key == "Enter") {
         addItem();
     }
 })
 
+
+
+
 function addItem() {
-    const item =" - " + document.getElementById("one-item").value;
+    const item = document.getElementById("one-item").value;
     const h1 = document.createElement("h1");
+    const groceryListContainer = document.getElementById("grocery-list");
+    const listItemNode = document.createElement("li");
+
+    listItemNode.appendChild(document.createTextNode(item));
 
     document.getElementById("one-item").value = '';
-    // this will append the food that is entered into the input box and placed into a list below after "enter Key" is clicked
-    h1.appendChild(document.createTextNode(item));
+    // this will append the value that is entered into the input box and placed into a list below after "enter Key" is clicked
+    groceryListContainer.appendChild(listItemNode);
 
+    // //crossing out each item when the item is clicked
+    // h1.addEventListener("click", function () {
+    //     h1.style.textDecoration = "line-through";
+    // })
+    listItemNode.addEventListener('click', function (evt) {
+        evt.target.classList.add('removed');
+    });
 
-    //crossing out each item when the item is clicked
-    h1.addEventListener("click", function () {
-        h1.style.textDecoration = "line-through";
-    })
+    // let div = document.getElementById("grocery-list");
+    // div.insertAdjacentElement("beforeend", h1);
 
-    let div = document.getElementById("all-items");
-    div.insertAdjacentElement("beforeend", h1);
+    groceryList.push(item);
+
 
 }
 
@@ -40,18 +58,27 @@ function trashList() {
     while (i < list.childNodes.length) {
         list.removeChild(list.childNodes[i]);
     }
-    
-    localStorage.setItem('groceryList', list.innerHTML);
+
+    localStorage.removeItem('grocery-list');
+    // this resets the grocery list array
+    groceryList = [];
+
 
 }
-// figure out how to archive user's list by saving it via local storage. This will be helpful when internet is slow or down. 
-// function archiveList() {
-//     let list = document.getElementById("all-items");
-//     const data = {
-//         groceryItem: archiveList.value
-//     }
+// figure out how to archive user's list by saving it via local storage. This will be helpful when internet is slow or down.
+function saveList() {
 
-// }
+    // save localStorage
+    const stringifiedGroceryList = JSON.stringify(groceryList);
 
-// save localStorage
-// localStorage.setItem('groceryList', list.innerHTML);
+
+    // saving list to local storage
+    localStorage.setItem('grocery-list', stringifiedGroceryList);
+
+}
+
+const saved = localStorage.getItem('grocery-list');
+
+if (saved) {
+    itemName.innerHTML = saved;
+}
